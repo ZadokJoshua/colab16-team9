77ff5f13712bddccb851f315af21db6d9fb6a4a5
Zadok Joshua <Ayegbazadok@outlook.com>
2023-01-12 23:57:17 +0100

    Add required folders.

---
+++ DrCarQuotes_Data/DbContexts/DrCarQuotesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DrCarQuotes_Data.DbContexts;
+
+public class DrCarQuotesDbContext : DbContext
+{
+	public DrCarQuotesDbContext(DbContextOptions<DrCarQuotesDbContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ DrCarQuotes_WebAPI/Controllers/RepairsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrCarQuotes_WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RepairsController : ControllerBase
+{
+    // GET: api/<RepairsController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<RepairsController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<RepairsController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+
+    }
+
+    // PUT api/<RepairsController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<RepairsController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
